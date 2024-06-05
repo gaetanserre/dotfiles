295ebfc7a177bfcc363e78d294ccf152144da92d
Gaëtan Serré <gaetan.serre@ens-paris-saclay.fr>
2024-06-05 18:22:49 +0200

    fr updates

---
+++ .config/waybar/scripts/update.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+get_nb_updates() {
+  num_updates=$(paru -Qu | wc -l)
+  if [ $num_updates -eq 0 ]; then
+    echo "À jour"
+  elif [ $num_updates -eq 1]; then
+    echo "$num_updates mise à jour"
+  else
+    echo "$num_updates mises à jour"
+  fi
+}
+
+update() {
+  paru && notify-send 'Le système a été mis à jour !'
+}
+
+if [[ "$1" == "--get" ]]; then
+  get_nb_updates
+elif [[ "$1" == "--update" ]]; then
+  update
+fi
